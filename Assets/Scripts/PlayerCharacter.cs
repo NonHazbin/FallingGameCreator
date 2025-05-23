fbc155088c9c097be7ec7c424a41d06f4b36bf9e
NonHazbin <non_hazbin@icloud.com>
2025-05-24 01:21:14 +0900

    自分の操作で移動するスクリプトの作成

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCharacter : CharacterBase
+{
+    [SerializeField, Header("WASDキーでのそうさ")]
+    private bool wasdControl = false;
+    [SerializeField, Header("矢印キーでのそうさ")]
+    private bool arrowControl = false;
+    protected override void Move()
+    {
+        float horizontal = 0f;
+        float vertical = 0f;
+
+        if (Keyboard.current != null)
+        {
+            if (wasdControl)
+            {
+                if (Keyboard.current.wKey.isPressed)
+                {
+                    vertical += 1;
+                }
+                if (Keyboard.current.sKey.isPressed)
+                {
+                    vertical -= 1;
+                }
+                if (Keyboard.current.aKey.isPressed)
+                {
+                    horizontal -= 1;
+                }
+                if (Keyboard.current.dKey.isPressed)
+                {
+                    horizontal += 1;
+                }
+            }
+            if (arrowControl)
+            {
+                if (Keyboard.current.upArrowKey.isPressed)
+                {
+                    vertical += 1;
+                }
+                if (Keyboard.current.downArrowKey.isPressed)
+                {
+                    vertical -= 1;
+                }
+                if (Keyboard.current.leftArrowKey.isPressed)
+                {
+                    horizontal -= 1;
+                }
+                if (Keyboard.current.rightArrowKey.isPressed)
+                {
+                    horizontal += 1;
+                }
+            }
+        }
+
+        moveDirection = new Vector2(horizontal, vertical).normalized;
+        transform.Translate(moveDirection * moveSpeed_ * Time.deltaTime);
+    }
+}
